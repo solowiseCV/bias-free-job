300d5dceb9ae632fb2e62b7cfae9f51f8db502ff
Dhalintin <uchexdhalitin@gmail.com>
2025-07-21 17:04:37 +0100

    Chore: modified the fetch request for jobs to only return neccessary info

--- src/features/jobSeeker/jobSeekerProfile/services/getJobSeeker.ts
+++ src/features/jobSeeker/jobSeekerProfile/services/getJobSeeker.ts
@@ -5,18 +5,12 @@ export class GetJobSeekerService {
   static async getJobSeekerByUserId(userId: string) {
     return await prisma.jobSeeker.findUnique({
       where: { userId },
-      include: {
-        user: true,
-      },
     });
   }
 
   static async getJobSeekerById(id: string) {
     return await prisma.jobSeeker.findUnique({
       where: { id },
-      include: {
-        user: true,
-      },
     });
   }
 }
